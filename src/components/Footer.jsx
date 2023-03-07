268114e36020eef9467bc671c1a119e038c77d17
ΔΙΟΝΥΣΙΟΣ ΚΑΜΜΕΝΟΣ <std080228@ac.eap.gr>
2023-03-07 09:46:59 +0200

    footer corrected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const today = new Date();
-const year = 2022; //today.getYear(); 
+const year = today.getFullYear(); 
 console.log(today.getYear());
 function Footer() {
     return (<div><footer><h1>Copyright  &copy; {year}</h1></footer></div>)
